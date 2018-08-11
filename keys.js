ca1719c5f7a84b309089b5f26fff4ab128c6e63f
Eric Eissler <ericeissler@Erics-MacBook-Pro.local>
2018-08-11 16:05:00 -0500

    final

---
+++ keys.js
@@ -0,0 +1,17 @@
+// a5b99bd4660041e0ada0d1e1abcdfe62 Spotify 
+// OMDb API: http://www.omdbapi.com/?i=tt3896198&apikey=4b790bdf
+
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+};
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
+
